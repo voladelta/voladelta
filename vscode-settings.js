6cf636e0c0a4557c939882a77d72a7a3e9a89068
σ Δ <91018286+voladelta@users.noreply.github.com>
2021-09-26 09:16:23 +0700

    Create vscode-settings.js

---
+++ vscode-settings.js
@@ -0,0 +1,77 @@
+{
+  "workbench.colorCustomizations": {
+    "[Ayu Mirage]": {
+      "editor.lineHighlightBackground": "#282E3D",
+      "editorSuggestWidget.selectedBackground": "#282E3D"
+    },
+    "editorBracketHighlight.foreground1": "#78716c",
+    "editorBracketHighlight.foreground2": "#d6d3d1",
+    "editorBracketHighlight.foreground3": "#f0abfc",
+    "editorBracketHighlight.foreground4": "#d946ef"
+  },
+  "editor.tokenColorCustomizations": {
+    "keywords": "#73736F",
+    "functions": "#F29E74",
+    "comments": "#B3A7FF",
+    "strings": "#96C25D",
+    "numbers": "#96C25D",
+    "textMateRules": [
+      {
+        "scope": "storage.type.function",
+        "settings": {
+          "foreground": "#73736F"
+        }
+      },
+      {
+        "scope": "constant.language, variable.language",
+        "settings": {
+          "foreground": "#96C25D",
+          "fontStyle": ""
+        }
+      },
+      {
+        "scope": "support.constant, support.class, entity.name.tag, keyword.control.flow",
+        "settings": {
+          "foreground": "#D97F68",
+          "fontStyle": ""
+        }
+      },
+      {
+        "scope": "meta.object.member, entity.other.attribute-name, support.type.property-name",
+        "settings": {
+          "foreground": "#A9A9A3"
+        }
+      },
+      {
+        "scope": "variable.other.object",
+        "settings": {
+          "foreground": "#999994"
+        }
+      },
+      {
+        "scope": "variable, variable.parameter",
+        "settings": {
+          "foreground": "#B4B4AE"
+        }
+      },
+      {
+        "scope": "meta.brace.square, punctuation.definition.string.begin, punctuation.definition.string.end, punctuation.definition.binding-pattern.object, punctuation.definition.binding-pattern.array, punctuation.definition.block, meta.brace.round, punctuation.terminator, punctuation.definition.tag.begin, punctuation.definition.tag.end, punctuation.section.embedded, punctuation.definition.parameters.begin, punctuation.definition.parameters.end",
+        "settings": {
+          "foreground": "#73736F"
+        }
+      },
+      {
+        "scope": "comment",
+        "settings": {
+          "fontStyle": ""
+        }
+      },
+      {
+        "scope": "entity.name",
+        "settings": {
+          "foreground": "#9dd2bb"
+        }
+      }
+    ]
+  }
+}
